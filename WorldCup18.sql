b4dd1aaec707e132c2b39a1b6bd9cb761f7735f1
Lionel Fortier <lionel@fortier.fr>
2018-06-06 23:24:23 +0200

    .SQL Correction

--- WorldCup18.sql
+++ WorldCup18.sql
@@ -11,7 +11,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-PronosCDM
+USE WorldCup18;
 --
 -- Base de données :  `WorldCup18`
 -- User : pronos
